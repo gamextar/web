734b36b5cb9563c1e83f6a59868030383d085361
Javier A. Fuchs <gamextar@hotmail.com>
2022-03-02 00:22:21 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,54 +1,3 @@
-$('.scroll').click(function(e) {
-    e.preventDefault();
-    var url = this.href;
-    var urlHash = this.hash;
-    var parts = url.split('#');
-    var trgt = parts[1];
-    var target_offset = $('#'+trgt).offset();
-    var target_top = target_offset.top;
-
-    $('html, body').animate({
-        scrollTop:target_top
-    }, 500);
-    if($('nav.dots a').hasClass('active')) {
-        $('nav.dots a').removeClass('active');
-    }
-    if (urlHash == '#section-1') {
-        $('nav.dots a.scroll-1').addClass('active');
-    }
-    if (urlHash == '#section-2') {
-        $('nav.dots a.scroll-2').addClass('active');
-    }
-    if (urlHash == '#section-3') {
-        $('nav.dots a.scroll-3').addClass('active');
-    }
-    if (urlHash == '#section-4') {
-        $('nav.dots a.scroll-4').addClass('active');
-    }
-});
-
-$('nav.dots a').click(function(){
-    if($('nav.dots a').hasClass('active')) {
-        $('nav.dots a').removeClass('active');
-    }
-    $(this).addClass('active');
-});
-
-$('.hamburger, nav.main ul li a').on('click', function(e) {
-    e.preventDefault();
-    $('.hamburger').toggleClass('is-open');
-    $('nav.main ul').toggleClass('is-open');
-});
-
-$('.geekstudios, .scroll-2').mouseenter(function() {
-    $('.hipster').addClass('show');
-});
-$('.geekstudios, .scroll-2').mouseleave(function() {
-    $('.hipster').removeClass('show');
-});
-
-
-
 <!-- Initialize Swiper -->
 
       var swiper = new Swiper('.swiper', 
@@ -155,3 +104,54 @@ var swiper2 = new Swiper('.swiper-container', {
     },
   });
 
+
+
+$('.scroll').click(function(e) {
+  e.preventDefault();
+  var url = this.href;
+  var urlHash = this.hash;
+  var parts = url.split('#');
+  var trgt = parts[1];
+  var target_offset = $('#'+trgt).offset();
+  var target_top = target_offset.top;
+
+  $('html, body').animate({
+      scrollTop:target_top
+  }, 500);
+  if($('nav.dots a').hasClass('active')) {
+      $('nav.dots a').removeClass('active');
+  }
+  if (urlHash == '#section-1') {
+      $('nav.dots a.scroll-1').addClass('active');
+  }
+  if (urlHash == '#section-2') {
+      $('nav.dots a.scroll-2').addClass('active');
+  }
+  if (urlHash == '#section-3') {
+      $('nav.dots a.scroll-3').addClass('active');
+  }
+  if (urlHash == '#section-4') {
+      $('nav.dots a.scroll-4').addClass('active');
+  }
+});
+
+$('nav.dots a').click(function()
+                    {
+  if($('nav.dots a').hasClass('active')) {
+      $('nav.dots a').removeClass('active');
+  }
+  $(this).addClass('active');
+});
+
+$('.hamburger, nav.main ul li a').on('click', function(e) {
+  e.preventDefault();
+  $('.hamburger').toggleClass('is-open');
+  $('nav.main ul').toggleClass('is-open');
+});
+
+$('.geekstudios, .scroll-2').mouseenter(function() {
+  $('.hipster').addClass('show');
+});
+$('.geekstudios, .scroll-2').mouseleave(function() {
+  $('.hipster').removeClass('show');
+});
